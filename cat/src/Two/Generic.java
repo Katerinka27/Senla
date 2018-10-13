802e8ba1c32429cdd7515e6fc27a4d997c75b4a2
Балицкая Екатерина <43970332+Katerinka27@users.noreply.github.com>
2018-10-13 16:30:00 +0200

    Update Generic.java

--- cat/src/Two/Generic.java
+++ cat/src/Two/Generic.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Generic {
 	Random rand = new Random();
 	private int min_value = 100;
-	private int max_value = 899;
+	private int max_value = 999;
 	public int rand()
 	{
 		int a = rand.nextInt(max_value-min_value)+min_value;
