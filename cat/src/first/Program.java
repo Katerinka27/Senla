d50b6c121ee26bf0a2418863ec7f8682ef695753
Балицкая Екатерина <43970332+Katerinka27@users.noreply.github.com>
2018-10-13 16:37:16 +0200

    Update Program.java

--- cat/src/first/Program.java
+++ cat/src/first/Program.java
@@ -3,7 +3,6 @@ package first;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Elements eL = new Elements();
 		Length LL = new Length();
 		LL.mas1(eL);
