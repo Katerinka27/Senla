bdeec5e642462e5ea3da80bffe044be9643c98b7
Балицкая Екатерина <43970332+Katerinka27@users.noreply.github.com>
2018-10-14 10:07:14 +0200

    Update Rose.java

--- cat/src/three/Rose.java
+++ cat/src/three/Rose.java
@@ -1,12 +1,7 @@
 package three;
 
 public  class Rose extends Flowers {
-	private int cost;
-	Rose(String name,int cost) {
-		super(name);
-		this.cost = cost;
-		}
-	public int getCost() {
-		return cost;
+		Rose(String name,int cost){
+		super(name,cost);
 	}
 }
