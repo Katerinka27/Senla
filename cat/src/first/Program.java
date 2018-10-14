c9815a63adf667531e09f96f6776c771b1329068
Балицкая Екатерина <43970332+Katerinka27@users.noreply.github.com>
2018-10-14 09:55:35 +0200

    Update Program.java

--- cat/src/first/Program.java
+++ cat/src/first/Program.java
@@ -4,8 +4,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		Elements eL = new Elements();
-		Length LL = new Length();
-		LL.mas1(eL);
+		Length leng = new Length();
+		leng.findLongestWord(eL);
 	}
-
 }
