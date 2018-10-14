ba7776d2c5e8b6016f5710eb8e2dcbf080a893a3
Балицкая Екатерина <43970332+Katerinka27@users.noreply.github.com>
2018-10-14 10:05:53 +0200

    Update Flowers.java

--- cat/src/three/Flowers.java
+++ cat/src/three/Flowers.java
@@ -2,9 +2,18 @@ package three;
 
 public abstract class Flowers {
 	private String name;
-	public abstract int getCost();
+	public int cost;
 	
-	public Flowers(String name){
+	public Flowers(String name,int cost){
 		this.name= name;
+		this.cost = cost;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int getCost(){
+		return cost;
 	}
 }
