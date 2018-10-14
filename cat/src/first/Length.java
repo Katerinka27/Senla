a7ef9c82b25e68fa1e7b355eb68497cb7f980cbc
Балицкая Екатерина <43970332+Katerinka27@users.noreply.github.com>
2018-10-14 09:59:15 +0200

    Update Length.java

--- cat/src/first/Length.java
+++ cat/src/first/Length.java
@@ -2,17 +2,15 @@ package first;
 
 public class Length {
 	
-	public void mas1(Elements eL) {
+	public void findLongestWord(Elements eL) {
 		int b = 0;
 		int c = 0;
-	for(int i = 0;i<eL.getlength();i++)
-	{
-		if (eL.getElMas(i).length()>b)
-		{
-			b = eL.getElMas(i).length();
+	for(int i = 0;i<eL.getlength();i++){		
+		if (eL.getElArray(i).length()>b){
+			b = eL.getElArray(i).length();
 			c = i;
 		}
 	}
-	System.out.println("Элемент с индексом - " + " " +c + ", " + "его длина " +b);
+	System.out.println("Элемент с индексом - " + eL.getElArray(c) + " max" + ", " +b);
 	}
 }
